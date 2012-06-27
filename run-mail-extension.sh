9c42d5e111a3e1193cf8f97e6d5b5e9df9cbb3b8
rafael liu <rliu@localhost.localdomain>
2012-06-27 16:12:47 -0300

    up

---
+++ run-mail-extension.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+JBOSS_DIR="/home/rliu/produtos/epp/jboss-epp-5.2.0/jboss-as"
+JBOSS_CONF="summit-sp"
+
+DATA_DIR="$JBOSS_DIR/server/$JBOSS_CONF/data"
+DEMO_DIR="$JBOSS_DIR/server/$JBOSS_CONF/deploy/demo"
+
+# kill previous instances
+killall -9 java 2> /dev/null
+
+# clean previous data and deployments
+rm -rf "$DATA_DIR" "$DEMO_DIR"
+mkdir "$DEMO_DIR"
+
+# copy packages
+cp "/home/rliu/Documents/authoring/GSS Bootcamp/demo/extension-mail/ear/target/extension-mail.ear" "$DEMO_DIR/"
+
+sudo service postfix start
+$JBOSS_DIR/bin/run.sh -c $JBOSS_CONF
